e85574ed0effe444533e2ef80641dc5458ee522b
raptorbox <raptorbox@gmail.com>
2016-02-02 16:23:50 +0300

    Все сбросил, записал templates для консольных приложений и WinForm+класс winForm

---
+++ Templates/ProjectTemplates/winform project/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+namespace $safeprojectname$
+{
+    /// <summary>
+    /// Summary description for Form1.
+    /// </summary>
+    public class Program
+    {
+       
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+
+    }
+}
