dbdcd5ab722c59795bdf1dd9f4b35610b0fa2ae0
raptorbox <raptorbox@gmail.com>
2016-02-03 14:57:06 +0300

    Упражнение с классами

---
+++ Class_exercise/Class_exercise/Class1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/// <summary>
+/// Пространсво имен нашего проекта
+/// </summary>
+namespace Class_exercise
+{
+    /// <summary>
+    /// Основной класс
+    /// </summary>
+    public class Class1
+    {
+        /// <summary>
+        /// Точка входа
+        /// </summary>
+        static void Main()
+        {
+            // создаем объекты через разные конструкторы
+            Point oPoint = new Point(10,5);
+            Point oPoint2 = new Point(); 
+            
+            //выводим данные 
+            oPoint.Print();
+            oPoint2.Print();
+
+            //считаем растояние от точки до центра координат
+            Console.WriteLine("Расстояние от точки до центра координат = {0:.####}", oPoint.Delta());
+
+            //Сложение точки с вектором
+            oPoint.Vector(5, 10);
+            oPoint.Print();
+
+            //используем set 
+            oPoint.X = 3;
+            oPoint.Y = 5;
+
+            //используем get
+            Console.WriteLine("Координаты точки:\n x = {0}\n y = {1}", oPoint.X, oPoint.Y);
+ 
+            oPoint.Scale = 3;
+            oPoint.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Class_exercise/Class_exercise/Class2.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Class_exercise
+{
+    /// <summary>
+    /// Класс Point для хранения точки и взаимодейсвием с ней
+    /// </summary>
+    /// <remarks>
+    /// включает в себя
+    /// Поля:
+    ///     int x, y;
+    ///Конструкторы, позволяющие создать экземпляр класса:
+    ///     с нулевыми координатами;
+    ///     с заданными координатами.
+    ///Методы, позволяющие:
+    ///     вывести координаты точки на экран;
+    ///     рассчитать расстояние от начала координат до точки;
+    ///     переместить точку на плоскости на вектор(a, b).
+    ///Свойства:
+    ///     получить-установить координаты точки(доступное для чтений и записи);
+    ///     позволяющие умножить координаты точки на скаляр(доступное только для записи).
+    /// </remarks>
+    class Point
+    {
+
+        /// <summary>
+        /// поля класса Point
+        /// </summary>
+        int x;
+        int y;
+
+        /// <summary>
+        /// Конструктор объекта со значениями по умолчанию
+        /// </summary>
+        public Point()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        /// <summary>
+        /// Конструктор объекта с заданными значениями
+        /// </summary>
+        /// <param name="x">Координата X</param>
+        /// <param name="y">Координата Y</param>
+        public Point(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>
+        /// Вывод координат точки
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine("Координаты точки:\n x = {0}\n y = {1}", x, y);
+        }
+
+        /// <summary>
+        /// Расчет растояния от центра координат до точки
+        /// </summary>
+        /// <returns>Расстояние от центра координат до точки</returns>
+        public double Delta()
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        /// <summary>
+        /// Перемещение точки на вектрор (a,b)
+        /// </summary>
+        /// <param name="a">Координата вектора X</param>
+        /// <param name="b">Координата векторв Y</param>
+        public void Vector(int a,int b)
+        {
+            this.x += a;
+            this.y += b;
+        }
+
+        /// <summary>
+        /// Координата Х точки
+        /// </summary>
+         public int X
+        {
+           
+            get
+            {
+
+                return x;
+            }
+
+            set
+            {
+                x = value;
+            }
+        }
+
+        /// <summary>
+        /// Координата Y точки
+        /// </summary>
+        public int Y
+        {
+
+            get
+            {
+
+                return y;
+            }
+
+            set
+            {
+                y = value;
+            }
+        }
+
+        /// <summary>
+        /// Умножение на скаляр
+        /// </summary>
+        public int Scale
+        {
+            set
+            {
+                x *= value;
+                y *= value;
+            }
+        }
+    }
+}
