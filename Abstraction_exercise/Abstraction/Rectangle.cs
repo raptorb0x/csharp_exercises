d5a9c5a6b4d02de7004dec80c622e604d58d3b93
raptorbox <raptorbox@gmail.com>
2016-02-04 14:51:45 +0300

    Сделал второй класс Reactangle - потомока абстрактого класса Figure

---
+++ Abstraction_exercise/Abstraction/Rectangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+    /// <summary>
+    /// Потомок абстракного класса Figure для реализации прямоугольника
+    /// </summary>
+    class Rectangle :Figure
+    {
+        /// <summary>
+        /// Поля класса
+        /// </summary>
+        private int x1;
+        private int y1;
+        private int x2;
+        private int y2;
+
+        /// <summary>
+        /// Глобальный конструктор класса 
+        /// </summary>
+        /// <param name="x1">Первая координата X</param>
+        /// <param name="y1">Первая координата Y</param>
+        /// <param name="x2">Вторая координата X</param>
+        /// <param name="y2">Вторая координата Y</param>
+        public Rectangle(int x1,int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        /// <summary>
+        /// Конструктор класса с значениями по умолчанию
+        /// </summary>
+        public Rectangle():this(0,0,1,1) { }
+
+        /// <summary>
+        /// Переопределение абстрактного метода подсчета площади для Rectangle
+        /// </summary>
+        /// <returns>Площадь</returns>
+        public override double Area()
+        {
+            return (x2 - x1) * (y2 - y1);
+        }
+
+        public override double Perimeter()
+        {
+            return (x2 - x1 + y2 - y1) * 2;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Прямоугольник  с кординатами {0},{1}:{2},{3} периметром {4:###} и площадью {5:###}", x1, y1, x2, y2, Perimeter(), Area());
+        }
+    }
+}
