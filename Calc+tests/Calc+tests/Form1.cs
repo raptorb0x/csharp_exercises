4ec26b8cc3a6cd2c74029e344928c46edbf3a6b9
raptorbox <raptorbox@gmail.com>
2016-02-12 10:15:18 +0300

    Вроде все готово

--- Calc+tests/Calc+tests/Form1.cs
+++ Calc+tests/Calc+tests/Form1.cs
@@ -13,14 +13,19 @@ namespace Calc_Forms
 {
     public partial class Form1 : Form
     {
-       
+        /// <summary>
+        /// Конструктор формы
+        /// </summary>
         public Form1()
         {
+            //Инициализируем форму
             InitializeComponent();
+
+            //Обновляем поле отображенияя ввода
             TextBoxRefresh();
         }
 
-
+        #region Изменения формы 
 
         /// <summary>
         /// Обновление Textbox
@@ -33,15 +38,21 @@ namespace Calc_Forms
         /// <summary>
         /// Пишем выражение в верхнем окне
         /// </summary>
-        /// <param name="Toadd"></param>
-        /// <param name="oper"></param>
+        /// <param name="Число"></param>
+        /// <param name="Символ оператора"></param>
         private void TextAdd( string Toadd, char oper)
         {
-
-            if (!textBox2.Text.Contains('+') && !textBox2.Text.Contains('-') && !textBox2.Text.Contains('*') && !textBox2.Text.Contains('/') && !textBox2.Text.Contains('='))
-            { textBox2.Text += " " + Toadd + " " + oper.ToString(); }
-            else if (!textBox2.Text.Contains('='))
-                    { textBox2.Text += " " + Toadd + " " + oper.ToString(); }
+            //Если символа выражения еще не было
+            if (!(textBox2.Text.Contains('+') || textBox2.Text.Contains('-') || textBox2.Text.Contains('*') || textBox2.Text.Contains('/')))
+                
+                //Пишем  число и оператор
+                textBox2.Text = " " + Toadd + " " + oper.ToString();
+                 
+            //Если на вводе символ посчета результата и его еще не было 
+            else if ((!textBox2.Text.Contains('=') && oper == '=') || (!textBox2.Text.Contains('%') && oper == '%'))
+                
+                //Пишем его
+                textBox2.Text += " " + Toadd + " " + oper.ToString(); 
 
         }
 
@@ -50,19 +61,28 @@ namespace Calc_Forms
         /// </summary>
         private void TextClr()
         {
+            //Если выражение содержит символа подсчета результата
             if (textBox2.Text.Contains("=") || textBox2.Text.Contains("%"))
+
+                //стираем его
                 textBox2.Text = "";
         }
 
         /// <summary>
         /// Принудительна очистка верхнего поля
         /// </summary>
-        /// <param name="force">Еслим истина- то очистка</param>
+        /// <param name="force">Если истина то очищаем</param>
         private void TextClr(bool force)
         {
             if (force)
                 textBox2.Text = "";
         }
+
+        #endregion
+
+        //-----------------------------------------------------------------------
+
+        #region Обработка ввода пользователя и взаимодействие с логикой
 
         /// <summary>
         /// Обработчик цифровой клавиши
@@ -73,17 +93,46 @@ namespace Calc_Forms
         {
             //Проверяем необходимость очистки верхнего поля
             this.TextClr();
+
             try
             {
                 //Отдаем в логику цифру
                 Logics.addDigit(sender.ToString().Last());
                 TextBoxRefresh();
             }
-            
+            //Если число слишком длинное
             catch (Exception)
             {
+                //Выводим всплывающее окошко с подсказкой
                 toolTip1.Show("Число слишком длинное", textBox1, 1000);
             }
+        }
+
+        /// <summary>
+        /// Обработчик смены знака
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bSigned_Click(object sender, EventArgs e)
+        {
+            //Отправляем в логику смену знака
+            Logics.Sign();
+            this.TextBoxRefresh();
+        }
+
+
+        /// <summary>
+        /// Обработчик запятой
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void bDiv_Click(object sender, EventArgs e)
+        {
+            this.TextClr();
+
+            //Предаем запятую в логику
+            Logics.Comma();
+            this.TextBoxRefresh();
         }
 
         /// <summary>
@@ -122,25 +171,17 @@ namespace Calc_Forms
         private void bReset_Click(object sender, EventArgs e)
         {
             TextClr(true);
+
+            //Сброс в логику
             Logics.Reset();
             this.TextBoxRefresh();
         }
 
         /// <summary>
-        /// Обработчик запятой
+        /// Обработчик нажатия на операцию
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void bDiv_Click(object sender, EventArgs e)
-        {
-            this.TextClr();
-
-            //Предаем запятую в логику
-            Logics.Comma();
-            this.TextBoxRefresh();
-        }
-
-
         private void bOper_Click(object sender, EventArgs e)
         {
             this.TextClr();
@@ -154,6 +195,11 @@ namespace Calc_Forms
 
         }
 
+        /// <summary>
+        /// Обработчик нажатиния на подсчет результата
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Equality_Click(object sender, EventArgs e)
         {
             TextClr();
@@ -164,49 +210,64 @@ namespace Calc_Forms
             this.TextBoxRefresh();
         }
 
-        private void bSigned_Click(object sender, EventArgs e)
-        {
-            Logics.Sign();
-            this.TextBoxRefresh();
-        }
-
+        /// <summary>
+        /// Обработчик нажатия на взятие кв. корня
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bSqrt_Click(object sender, EventArgs e)
         {
             try
             {
+                //Квадратный корень в лонику
                 Logics.Sqrt();
-                this.TextBoxRefresh();
             }
-
             catch (Exception)
             {
+                //Исключение
                 toolTip1.Show("Корень из отрицательного числа", textBox1, 1000);
-                this.TextBoxRefresh();
             }
+
+            this.TextBoxRefresh();
         }
 
+        /// <summary>
+        /// Нажатие на обратную пропорцианальность
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bRev_Click(object sender, EventArgs e)
         {
+            //Обратная пропорциональность в логику
             Logics.Inverse();
             this.TextBoxRefresh();
         }
 
+        /// <summary>
+        /// Вычисление через процент
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void bPerc_Click(object sender, EventArgs e)
         {
             try
             {
+                //Процент в логику
                 Logics.Percent();
                 TextClr();
                 TextAdd(this.textBox1.Text, '%');
-                this.TextBoxRefresh();
-            }
+             }
             catch(Exception)
             {
+                //Если исключение
                 toolTip1.Show("Процент от чего?", textBox1, 1000);
-                this.TextBoxRefresh();
             }
+
+            this.TextBoxRefresh();
         }
 
+        #endregion
 
+        //-----------------------------------------------------------------------
     }
 }
